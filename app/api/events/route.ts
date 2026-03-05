54e2ad47b11b35481d1aa243be9bfc314282dac5
maanav <maanavcchauhan@gmail.com>
2026-03-05 13:39:19 +0000

    Api Routes for GET and POST added.

---
+++ app/api/events/route.ts
@@ -0,0 +1,62 @@
+import {NextRequest, NextResponse} from "next/server";
+import {v2 as cloudinary} from "cloudinary";
+import connectToDatabase from "@/lib/mongodb";
+import Event from "@/database/event.model";
+
+export async function POST(req: NextRequest) {
+    try {
+        await connectToDatabase();
+        const formData = await req.formData();
+        // try to parse the form data into a JSON object
+        let event;
+        try {
+            event = Object.fromEntries(formData.entries());
+        } catch (e) {
+            // status 400: bad request
+            return NextResponse.json({message: "Invalid JSON Data Format."}, {status: 400});
+        }
+
+        const file = formData.get("image") as File;
+
+        if (!file) {
+            return NextResponse.json({message: "Image file is required."}, {status: 400})
+        }
+
+        // changing the format of the container holding the bytes.
+        // (kind of changing the lang to something clouditionary can read).
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult= await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({resource_type:'image',folder:'DevEvent'},(error, result) => {
+                if (error) {
+                    return reject(error);
+                } else {
+                    resolve(result);
+                }
+            }).end(buffer);
+        })
+
+        event.image = (uploadResult as {secure_url: string }).secure_url;
+        const createdEvent = await Event.create(event);
+        // console.log(formData);
+        // status 201: created
+        return NextResponse.json({message: "Event Created Successfully.", event: createdEvent}, {status: 201});
+
+
+    } catch (e){
+        console.error(e);
+        // status 500: internal server error
+        return NextResponse.json({message: "Event Creation Failed.", error: e instanceof Error ? e.message: "Unknown"}, {status:500});
+    }
+}
+
+export async function GET(req: NextRequest) {
+    try {
+        await connectToDatabase();
+        const events = await Event.find().sort({createdAt:-1});
+        return NextResponse.json({message: "Event Fetched Successfully.", events}, {status: 200})
+    } catch (e) {
+        return NextResponse.json({message: "Event Fetching Failed.", error:e}, {status: 500})
+    }
+}
--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,26 +1,26 @@
 import ExploreBtn from "@/components/ExploreBtn";
-import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
+import EventCard from "@/components/EventCard";
+import {notFound} from "next/navigation";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-const Page = async ()  => {
-    // Promise of a response.
-    const response = await fetch(`${BASE_URL}/api/events`);
-    // Promie of JSON Parsed (like unpacking an object).
-    // if (!response.ok) {
-    //     throw new Error("Failed to fetch events");
-    // }
+const EventDetailsPage = async ({params}:{params:Promise<{slug:string}>}) => {
+    // Destructure the slug from the params object.
+    const {slug} = await params;
+    const request = await fetch(`${BASE_URL}/api/events/${slug}`);
+    const {event} = await request.json();
+    if (!event) return notFound();
 
-    const {events} = await response.json();
+
     return (
-        <section>
+        <section id="event">
             <h1 className="text-center pb-1">The Hub for Every Dev <br /> Event you can't miss</h1>
             <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place.</p>
             <ExploreBtn />
             <div className="mt-20 space-y-7">
                 <h3>Featured events</h3>
                 <ul className='events'>
-                    {events && events.length>0 && events.map((event: IEvent) => (
+                    {event && event.length>0 && event.map((event: IEvent) => (
                         <li key={event.title}>
                             <EventCard {...event} />
                         </li>
@@ -31,4 +31,4 @@ const Page = async ()  => {
 
     )
 }
-export default Page
+export default EventDetailsPage
